274db419b29cbbf8dcc59f3bf3f1b3a8a8fab46c
kebv12 <rrainer767@gmail.com>
2026-04-03 23:23:20 -0500

    5to avance

--- src/gui/v1.java
+++ src/gui/v1.java
@@ -6,11 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class v1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JTextField txtLibro;
+	private JLabel lblNewLabel_1;
+	private JButton btnRegistrar;
+	private JTextArea txtS;
 
 	/**
 	 * Launch the application.
@@ -37,7 +47,34 @@ public class v1 extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		{
+			lblNewLabel = new JLabel("Sistema de Gestión Integrado para Bibliotecas Universitarias");
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			lblNewLabel.setBounds(42, 11, 398, 40);
+			contentPane.add(lblNewLabel);
+		}
+		{
+			txtLibro = new JTextField();
+			txtLibro.setBounds(91, 71, 86, 20);
+			contentPane.add(txtLibro);
+			txtLibro.setColumns(10);
+		}
+		{
+			lblNewLabel_1 = new JLabel("Ingresar Libro");
+			lblNewLabel_1.setBounds(14, 74, 67, 14);
+			contentPane.add(lblNewLabel_1);
+		}
+		{
+			btnRegistrar = new JButton("Registrar Libro");
+			btnRegistrar.setBounds(42, 98, 120, 23);
+			contentPane.add(btnRegistrar);
+		}
+		{
+			txtS = new JTextArea();
+			txtS.setBounds(10, 127, 447, 166);
+			contentPane.add(txtS);
+		}
 
 	}
-
 }
